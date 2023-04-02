21f85b8d49e5f01c446d335f7c0aee0cbffabee0
Ilya Yalymov <chezeee@gmail.com>
2023-04-03 00:04:09 +0300

    added saving images on the client side using LocalStorage. Added element styles.

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,75 @@
 import { getLoader } from './loader.js';
 
-const uploadForm = document.querySelector('.upload-form');
+// const uploadForm = document.querySelector('.upload-form');
 const file = document.querySelector('#upload-image');
-const uploadBtn = document.querySelector('.upload-btn');
+const showImagesBtn = document.querySelector('.upload-btn');
 const output = document.querySelector('#output');
 
-uploadBtn.addEventListener('click', e => {
-  e.preventDefault();
+//Загрузкa изображений в localStorage на стороне клиента
+function uploadImage(event) {
+  const reader = new FileReader();
+  const name = event.target.files[0].name;
 
-  getLoader(uploadForm);
-  const newImage = document.createElement('img');
+  reader.addEventListener('load', function () {
+    if (this.result && localStorage) {
+      window.localStorage.setItem(name, this.result);
+    } else {
+      alert();
+    }
+  });
+  reader.readAsDataURL(event.target.files[0]);
+}
 
-  newImage.setAttribute('src', `${URL.createObjectURL(file.files[0])}`);
+function deleteImage(elem) {
+  window.localStorage.removeItem(elem);
+}
 
-  output.append(newImage);
+//Выгрузка изображений из localStorage. Вывод на экран
+function showImages() {
+  const div = document.querySelector('.file-wrapper');
+
+  if (output.contains(div)) {
+    output.append(div);
+  } else {
+    for (let i = 0; i < localStorage.length; i++) {
+      const result = window.localStorage.getItem(window.localStorage.key(i));
+      const div = document.createElement('div');
+      const image = new Image();
+      const deleteBtn = document.createElement('button');
+
+      deleteBtn.textContent = 'X';
+      deleteBtn.classList.add('delete-btn');
+      deleteBtn.addEventListener('click', event => {
+        deleteImage(`${window.localStorage.key(i)}`);
+        event.target.parentElement.remove();
+      });
+
+      div.classList.add('file-wrapper');
+      div.append(image);
+      div.append(deleteBtn);
+
+      image.src = result;
+      image.setAttribute('class', 'img');
+      image.setAttribute('size', '50');
+
+      output.append(div);
+    }
+
+    showImagesBtn.classList.add('disabled');
+  }
+}
+
+showImages();
+
+file.addEventListener('change', event => {
+  uploadImage(event);
+  showImagesBtn.classList.remove('disabled');
+});
+
+showImagesBtn.addEventListener('change', event => {
+  event.preventDefault();
+  showImages();
+  // getLoader(uploadForm);
+  // showImages();
+  // showImagesBtn.setAttribute('disabled', true);
 });
